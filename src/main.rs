85cf6cefb36e6fb354bef53b4b0871bacde87e47
Miguel Lattuada <miguellattuada@outlook.com>
2022-10-25 20:11:46 -0300

    fix: make it work

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use futures::{
     future::BoxFuture,
     task::{waker_ref, ArcWake},
 };
-use reqwest::{Client, Error, Response};
 use std::{
     future::Future,
     sync::{
@@ -14,17 +13,18 @@ use std::{
     thread::sleep,
     time::Duration,
 };
+use surf::{Error, Response};
 
-type ReqwestFuture = Result<Response, Error>;
+type RequestFuture = Result<Response, Error>;
 
 struct HttpTask {
-    future: Mutex<Option<BoxFuture<'static, ReqwestFuture>>>,
+    future: Mutex<Option<BoxFuture<'static, RequestFuture>>>,
     notify: SyncSender<Arc<HttpTask>>,
 }
 
 impl HttpTask {
     pub fn from_future(
-        future: impl Future<Output = ReqwestFuture> + 'static + Send,
+        future: impl Future<Output = RequestFuture> + 'static + Send,
         notify: SyncSender<Arc<HttpTask>>,
     ) -> Self {
         Self {
@@ -60,15 +60,17 @@ impl BatchHttpExecutor {
             sender,
         }
     }
-
+    
+    /**
+     * Create tasks based on config batch
+     * Send those tasks thorugh channel
+     * Drop channel sender
+     */
     pub fn spawn(self) -> Self {
-        let client = Client::new();
-
         let mut tasks = (0..self.batch_config.number_of_requests).map(|_| {
-            let url = &self.batch_config.url;
-
-            let request = client.get(url).send();
-            HttpTask::from_future(async move { request.await }, self.sender.clone())
+            let url = self.batch_config.url.to_owned();
+            let future = async { surf::get(url).send().await };
+            HttpTask::from_future(future, self.sender.clone())
         });
 
         while let Some(task) = tasks.next() {
@@ -76,9 +78,45 @@ impl BatchHttpExecutor {
             self.sender.send(safe_task).ok();
         }
 
+        // Do not expect more messages
+        drop(&self.sender);
+
+        // let url = &self.batch_config.url;
+
+        // let future = async {
+        //     println!("making request");
+        //     surf::get("http://google.com").await
+        // };
+
+        // let task = HttpTask {
+        //     future: Mutex::new(Some(Box::pin(future))),
+        //     notify: self.sender.clone(),
+        // };
+
+        // self.sender.send(Arc::new(task)).ok();
+
+        // drop(self.sender);
+
+        // while let Ok(task) = self.receiver.recv() {
+        //     let mut locked_future = task.future.lock().unwrap();
+        //     if let Some(mut future) = locked_future.take() {
+        //         let waker = waker_ref(&task);
+        //         let context = &mut Context::from_waker(&*waker);
+
+        //         if future.as_mut().poll(context).is_pending() {
+        //             *locked_future = Some(future);
+        //         }
+        //     }
+        // }
         self
     }
 
+    /**
+     * Loop over the received tasks through the channel
+     * Take the future from the task
+     * Poll the future
+     * Put it back if it's still pending
+     */
     pub fn run(self) -> Self {
         while let Ok(task) = self.receiver.recv() {
             let mut locked_future = task.future.lock().unwrap();
@@ -134,9 +172,11 @@ impl LoadTestBuilder {
     pub fn execute(self) -> Self {
         if self.duration > 0 && self.rate > 0 {
             let mut sec_spent = 0;
+
             loop {
                 if sec_spent <= self.duration {
-                    BatchHttpExecutor::new(&self.url, &self.rate).spawn().run();
+                    BatchHttpExecutor::new(&self.url, &self.rate).spawn();
+                    // BatchHttpExecutor::new(&self.url, &self.rate).spawn().run();
 
                     sleep(Duration::from_secs(1));
                     sec_spent += 1;
@@ -152,9 +192,9 @@ impl LoadTestBuilder {
 
 fn main() {
     let matches = command!()
-        .arg(arg!(--url <URL> "define load test URL").required(true))
-        .arg(arg!(--rate <RATE> "requests per second").default_value("5"))
-        .arg(arg!(--duration <DURATION> "load test duration in seconds").default_value("10"))
+        .arg(arg!(-u --url <URL> "define load test URL").required(true))
+        .arg(arg!(-r --rate <RATE> "requests per second").default_value("5"))
+        .arg(arg!(-d --duration <DURATION> "load test duration in seconds").default_value("10"))
         .get_matches();
 
     let mut builder = LoadTestBuilder::new();
@@ -163,12 +203,12 @@ fn main() {
         builder.url(url.to_owned());
     }
 
-    if let Some(rate) = matches.get_one::<u8>("rate") {
-        builder.rate(*rate);
+    if let Some(rate) = matches.get_one::<String>("rate") {
+        builder.rate(rate.parse::<u8>().unwrap());
     }
 
-    if let Some(duration) = matches.get_one::<u8>("duration") {
-        builder.duration(*duration);
+    if let Some(duration) = matches.get_one::<String>("duration") {
+        builder.duration(duration.parse::<u8>().unwrap());
     }
 
     builder.execute();
